918d1e4138e103f2b31b9725ea6bfe5995d69c18
esgowens <kinggsone@gmail.com>
2020-09-07 14:09:46 -0400

    lesson end

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,5 @@
 let font
+let graphic
 
 function preload () {
     font = loadFont("assets/spacegrotesk-medium.otf")
@@ -6,14 +7,21 @@ function preload () {
 
 function setup() {
 createCanvas(500, 500)
+
+graphic = createGraphics(500, 500)
+
+graphic.fill("#ef5236")
+graphic.textFont(font)
+graphic.textSize(400)
+graphic.textAlign(CENTER, CENTER)
+graphic.text("01", width / 2, height / 2)
 }
 
 function draw () {
     background("#ecf072")
 
-    fill("#ef5236")
-    textFont(font)
-    textSize(400)
-    textAlign(CENTER, CENTER)
-    text("01", width / 2, height / 2)
+    let val = sin(frameCount * 0.03) * 250
+
+    copy(graphic, 0, 0, 500, 500, 0, 0, 250 + val, 500)
+    copy(graphic, 0, 0, 500, 500, 250 + val, 0, 250 - val, 500)
 }
